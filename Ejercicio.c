4aed5a16d141895bd2dc91a65bc08db381d470ef
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2023-11-21 15:13:44 -0500

    Se incluyeron dos for con los que se imprimiria una sola matris de ceros

--- Ejercicio.c
+++ Ejercicio.c
@@ -7,4 +7,17 @@ scanf("%d", &n);
 printf("Coloque los datos de m\n");
 scanf("%d", &m);
 int matrix[n][m];
+for (int i = 0; i < n; i++)
+{
+    printf("\n");
+    for (int j = 0; j < m; j++)
+    {           
+                         
+        matrix[i][j]=0;
+        printf("%d ", matrix[i][j]);
+        
+    }
+    
+    
+}
 }
